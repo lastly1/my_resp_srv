31c9337b0c6c461d0264f3b2f3d41d4494eb1921
Ilya <6147459@gmail.com>
2022-12-19 18:49:33 +0300

    first commit

---
+++ scraping_service/views.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'Ilyusha'
+    _context = {'date':date, 'name': name}
+    return render(request, 'home.html', _context)
